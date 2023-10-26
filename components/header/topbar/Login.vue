05f7dc02e22a9e479c91ee6304f2d0b8a2172276
khusnutdinovilm <khusnutdinov.ilmir.w7@gmail.com>
2023-10-26 08:58:34 +0500

    добавил dropdown-menu для блока с информацией о пользователе. разработал страницу profile.vue

--- components/header/topbar/Login.vue
+++ components/header/topbar/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="user-data">
+  <div class="user-data" @click.prevent="isProfileMenuOpen = !isProfileMenuOpen">
     <div class="user-data__name">Даша Казанцева</div>
 
     <div class="user-data__photo">
@@ -9,14 +9,28 @@
     <div class="user-data__arrow"><i class="far fa-angle-down" /></div>
   </div>
 
-  <div class="topbar__profile-menu" data-show="0">
+  <div class="topbar__profile-menu" :data-show="isShowProfileMenu">
     <div class="profile-menu">
       <ul class="select-lists block profile-menu__select-list">
-        <!-- <li class="select-lists__item"><a href="#">Профиль</a></li> -->
-        <!-- <li class="select-lists__item top-bar"><a href="/profile">Редактирование</a></li> -->
-        <!-- <li class="select-lists__item"><a href="?section=settings">Настройки</a></li> -->
+        <li class="select-lists__item">
+          <nuxt-link to="/profile">Профиль</nuxt-link>
+        </li>
+        <li class="select-lists__item top-bar">
+          <nuxt-link to="/">Редактирование</nuxt-link>
+        </li>
+        <li class="select-lists__item">
+          <nuxt-link to="/manager?section=settings">Настройки</nuxt-link>
+        </li>
         <li class="select-lists__item top-bar">Выход</li>
       </ul>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const isProfileMenuOpen = ref(false);
+
+const isShowProfileMenu = computed(() => (isProfileMenuOpen.value ? "1" : "0"));
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h2>Profile</h2>
+  </main>
+</template>
