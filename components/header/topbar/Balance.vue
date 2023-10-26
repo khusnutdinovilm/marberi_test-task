094601c6f0031c739c9214d7933092ede15e595a
khusnutdinovilm <khusnutdinov.ilmir.w7@gmail.com>
2023-10-26 08:47:36 +0500

    добавил открытие и закрытие dropdown-menu для карточки баланка в хэдере

--- components/header/topbar/Balance.vue
+++ components/header/topbar/Balance.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="topbar-balance">
-    <div class="topbar-balance__control">
+    <div class="topbar-balance__control" @click.prevent="isBalanceMenuOpen = !isBalanceMenuOpen">
       <button class="button" data-color="purple" data-tippy-content="Основной баланс">
         <i class="fas fa-ruble-sign" />
       </button>
       0 <i class="fas fa-ruble-sign" />
     </div>
 
-    <div class="topbar-balance__profile-menu" data-show="0">
+    <div class="topbar-balance__profile-menu" :data-show="isShowBalanceMenu">
       <div class="profile-menu block">
         <div class="topbar-balance__balance">
           Основной баланс
@@ -20,3 +20,11 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const isBalanceMenuOpen = ref(false);
+
+const isShowBalanceMenu = computed(() => (isBalanceMenuOpen.value ? "1" : "0"));
+</script>
